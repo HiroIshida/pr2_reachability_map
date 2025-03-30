a1604f26411971f3afcb49df4730f0f0544a7d9f
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2025-03-31 02:03:36 +0900

    Initialize

---
+++ pr2_reachability_map/model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+
+class FCN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(FCN, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 50)
+        self.fc2 = nn.Linear(50, 50)
+        self.fc3 = nn.Linear(50, 50)
+        self.fc4 = nn.Linear(50, output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pr2_reachability_map",
+    version="0.0.0",
+    description="experimental",
+    author="Hirokazu Ishida",
+    install_requires=["numpy", "torch"],
+    packages=["pr2_reachability_map"],
+)
